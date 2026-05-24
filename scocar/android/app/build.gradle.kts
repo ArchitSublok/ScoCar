55999cde39b225a0ef04b588eaab7e13029efe2d
Archit Sublok <architsublok@gmail.com>
2026-05-24 17:29:51 +0530

    ok

--- scocar/android/app/build.gradle.kts
+++ scocar/android/app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.scocar"
+    namespace = "com.example.soccar"
     // compileSdk = flutter.compileSdkVersiontoInt()
     compileSdk = 35
     // ndkVersion = flutter.ndkVersion
